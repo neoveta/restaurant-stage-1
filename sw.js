c83528928ac725fcf3097d5e807c6581513e1bf0
Ruslana Solovova <ruslanasolovova@gmail.com>
2018-10-28 08:32:01 -0700

    installed and activated service worker

--- sw.js
+++ sw.js
@@ -1,45 +1,47 @@
-console.log('Service Worker: Registered');
+var cache_name = 'resturant-cache';
 
+// install 
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-            './',
-            './index.html',
-            './restaurant.html',
-            './css/styles.css',
-            './data/restaurants.json',
-            './js/dbhelper.js',
-            './js/main.js',
-            './js/restaurant_info.js',
-            './js/sw_register.js',
-            './img/1.jpg',
-            './img/2.jpg',
-            './img/3.jpg',
-            './img/4.jpg',
-            './img/5.jpg',
-            './img/6.jpg',
-            './img/7.jpg',
-            './img/8.jpg',
-            './img/9.jpg',
-            './img/10.jpg'
-        ]);
-      })
-    );
-  }); 
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          const responseClone = response.clone();   //clone to return and cache
-  
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        });
-      }
-    }));
-  });
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cache_name).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
+      ]);
+    })
+  );
+});
+
+// activate
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// fetch
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
